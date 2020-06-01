66f84569ed4e27091afe52cf9c9abbf816882e3b
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-01 13:52:18 +0100

    learning module

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -2,15 +2,15 @@
 
 output "website_bucket_arn" {
   description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
+  value       = module.s3.arn
 }
 
 output "website_bucket_name" {
   description = "Name (id) of the bucket"
-  value       = module.website_s3_bucket.name
+  value       = module.s3.name
 }
 
 output "website_endpoint" {
   description = "Domain name of the bucket"
-  value       = module.website_s3_bucket.website_endpoint
+  value       = module.s3.website_endpoint
 }
